968dc6c124e7eb349991213dfb7a1f52ac2e609f
tuquangnam07 <tuquangnamht2007@gmail.com>
2025-04-14 00:24:35 +0700

    Create adminNotice.js

---
+++ Locket_crack/adminNotice.js
@@ -0,0 +1,6 @@
+/***********************************************
+> Admin Notice by Từ Quang Nam
+***********************************************/
+if (!$done) var $done = (obj) => {};
+$notification.post("💬 Thông báo từ Từ Quang Nam", "Cảm ơn bạn đã sử dụng bản Mod", "Không bán hoặc chia sẻ công khai nhé!");
+$done({});
