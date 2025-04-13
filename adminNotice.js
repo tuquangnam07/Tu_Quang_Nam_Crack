873249a3b3a1edca5ab547f517d34b83699fb849
tuquangnam07 <tuquangnamht2007@gmail.com>
2025-04-13 22:28:19 +0700

    Create adminNotice.js

---
+++ adminNotice.js
@@ -0,0 +1,4 @@
+console.log("📢 Thông báo từ Admin:");
+console.log("👉 Theo dõi Telegram: @TenKenh để nhận update mới nhất!");
+console.log("❤️ Cảm ơn bạn đã sử dụng module Locket-Ohoang7_Gold!");
+$done({});
