a38ce3700146836d64a342b5ddeaddf2b9966eb5
tuquangnam07 <tuquangnamht2007@gmail.com>
2025-04-14 14:03:03 +0700

    Create adminNotice.js

---
+++ TEST/locket/Locket_crack/adminNotice.js
@@ -0,0 +1,11 @@
+/***********************************************
+> Thông báo Admin - By Từ Quang Nam
+***********************************************/
+if (typeof $notification !== "undefined") {
+  $notification.post(
+    "🌟 Chào mừng bạn đến với bản Mod Locket Gold",
+    "📢 Admin: Từ Quang Nam",
+    "👉 Không chia sẻ công khai để tránh bị fix!\n💎 Chúc bạn sử dụng vui vẻ!"
+  );
+}
+$done({});
